c66cc9ef07fbf331abbd7555f86048790a776a05
Walter Samuel Rodriguez Stand <waltersamrodriguez@gmail.com>
2024-11-28 15:58:45 -0500

    first commit

---
+++ ejercicio.py
@@ -0,0 +1,8 @@
+# Configura un entorno de pruebas local para tu proyecto usando pytest. 
+# Escribe una prueba sencilla que verifique que la función sumar(a, b) 
+# devuelve la suma correcta de dos números.
+
+# Crea un archivo de configuración de CI para GitHub Actions en tu proyecto
+# y haz que se ejecute pytest automáticamente cada vez que realices un push
+# a la rama main. Asegúrate de que los resultados de las pruebas se muestren
+# correctamente en la interfaz de GitHub.
